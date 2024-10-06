c4b32b9843cf520983edf3726208b0eb4355d129
Bruno Nicanor <bruno.hero01@gmail.com>
2024-10-06 10:44:16 -0300

    Trabalhando com listas

--- src/Principal/PrincipalComListas.java
+++ src/Principal/PrincipalComListas.java
@@ -1,4 +1,49 @@
 package Principal;
 
+import br.com.thinksolutions.ScreemMatch.Modelo.Filme;
+import br.com.thinksolutions.ScreemMatch.Modelo.Series;
+import br.com.thinksolutions.ScreemMatch.Modelo.Titulo;
+
+import java.util.ArrayList;
+
 public class PrincipalComListas {
+    public static void main(String[] args) {
+
+        //Criando objetos
+        var filme1 = new Filme("Star Wars", 1978);
+        var filme2 = new Filme("Vingadores", 2012);
+        var serie1 = new Series("Dark", 2018, 60, 8);
+
+        //Avaliando os conteúdos
+        filme1.avaliar(8);
+        filme2.avaliar(9);
+        serie1.avaliar(10);
+
+        //Criando lista de filmes
+        ArrayList<Titulo> lista = new ArrayList<>();
+
+        lista.add(filme1);
+        lista.add(filme2);
+        lista.add(serie1);
+
+        //Percorrendo a lista
+        /*for(Titulo item: lista){
+            System.out.println(item.getNome());
+
+            //Atribuindo a referencia de item (variável que está sendo usada para
+            percorrer a lista) em uma variável do tipo Filme, pois Titulo é genérica
+            demais para usar o método getClassificacao() que existe em filme e série.
+            Também é feito o casting, pois uma variável do tipo Filme não pode recer
+            um Titulo (item).
+            Filme filme = (Filme) item;
+            System.out.println("Classsificação: " + filme.getClassificacao());
+        }*/
+
+        //Resumo do código acima
+        for(Titulo item: lista){
+            System.out.println(item.getNome());
+            if(item instanceof Filme filme)
+                System.out.println("Classsificação: " + filme.getClassificacao());
+        }
+    }
 }
